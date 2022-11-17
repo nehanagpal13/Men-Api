52b6c50732ba339e8734e959cee6df6bce7b1a16
Neha Nagpal <nehanagpal.cybernext@gmail.com>
2022-11-17 14:44:19 +0530

    Entry point for Men-Api

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors=require('cors');
 
 require('dotenv').config();
 require('./Config/db');
 
 
-//mongodb+srv://MEN_Api_Neha:<password>@cluster0.dy4blsl.mongodb.net/?retryWrites=true&w=majority
-
-//MEN_Api_Neha  , password: men1234
-
-// mongoose.connect(process.env.DB_URI,
-//     {
-//     dbName: process.env.DB_NAME,
-//     user : process.env.DB_USER,
-//     pass: process.env.DB_PASS,
-//     useNewUrlParser: true
-
-// })
-// .then(()=>{
-//     console.log('Connected to Mongodb');
-
-// }).catch((err)=>{
-//     console.log(err.message);
-// });
-
-// const db=mongoose.connection;
-// db.on('error',()=>{
-//     console.log('Error occured from DB');
-
-// });
-
-// db.on('open',()=>{
-//     console.log('Successfully accessed the database DB!');
-
-// });
 
 
 //MIDDLEWARES
+
+app.use(cors());
+//we set up the middleware to parse a body
+//Body parser
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 app.use(require('./Routes'));
+
+
 
 // app.use('',(req,res,next)=>{
 //     console.log('API Middleware');
@@ -50,18 +31,10 @@ app.use(require('./Routes'));
 //     next();
 // });
 
-//ROUTE
-// app.get('/',(req,res)=>{
-//     res.send('app.js INDEX Page')
-// });
-
-// app.get('/api',(req,res)=>{
-//     res.send('API Page')
-// });
 
 PORT= process.env.PORT || 4000;
 
-app.listen(3000,()=>{
-    console.log(`Listening on http://localhost:3000 port ${PORT}....`);
+app.listen(PORT,()=>{
+    console.log(`Listening on http://localhost:${PORT} port ....`);
 });
 

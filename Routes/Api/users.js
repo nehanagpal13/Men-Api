76350bfce9d78e6117d9feb44275d044af6e34a3
Neha Nagpal <nehanagpal.cybernext@gmail.com>
2022-11-17 14:43:48 +0530

    Routing path to DB CRUD O/p s

--- Routes/Api/users.js
+++ Routes/Api/users.js
@@ -1,13 +1,30 @@
 const router= require('express').Router();
 const users_ctr= require('../../controllers/users');
+
 //ROUTE
 
 // router.get('/',(req,res) => {
 //     res.send('INDEX PAGE')
 // });
 
+//CRUD
 
-router.get('/',users_ctr.index);
+//CREATE
+//endpoint 
+//http://localhost:3000/api/users
+router.post('/',users_ctr.createUser);
+
+//READ
+router.get('/',users_ctr.listAll);
+router.get('/:id',users_ctr.singleUser);
+
+//UPDATE
+router.patch('/:id',users_ctr.updateUser);
+
+//DELETE
+router.delete('/',users_ctr.deleteUser);
+
+
 
 
 module.exports= router;
